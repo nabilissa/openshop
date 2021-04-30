04e15014a9fa9768dfc501bfe63128fe2e39c33f
nabil issa barro <isbarros@gmail.com>
2021-04-30 16:03:21 +0000

    config migrates

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-          \App\Models\User::factory(10)->create();
+          //\App\Models\User::factory(10)->create();
         $this->call([
+        RoleSeeder::class,
            ProduitSeeder::class,
            CategorySeeder::class,
-           RoleSeeder::class,
+
             //autres seeder
         ]);
     }
